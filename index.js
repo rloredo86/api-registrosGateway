95dccb5774a805ae60b369cf8c2d3f1f350ba2d5
rloredo86 <59886096+rloredo86@users.noreply.github.com>
2026-01-21 13:02:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const API_USER = 'admin';
 const API_PASSWORD = 'passwordSeguro123';
 
 // URLs de los Microservicios (Ajusta estos valores según tus servicios reales)
-const URL_SERVICE_USERS = 'https://apo-registro.paas.oracle-mty1.juanlopez.dev';
-const URL_SERVICE_CODES = 'https://apo-registro.paas.oracle-mty1.juanlopez.dev/';
-const URL_SERVICE_CONFIRM = 'https://apo-registro.paas.oracle-mty1.juanlopez.dev/';
+const URL_SERVICE_USERS = 'https://api-registros-reg.vercel.app/';
+const URL_SERVICE_CODES = 'https://api-registros-codigo.vercel.app/';
+const URL_SERVICE_CONFIRM = 'https://api-registro-confirmacion.vercel.app/';
 // --------------------------------------
 
 // 1. Seguridad Básica Global
